6ed15c396cf78ed435e078649f2630dcf71b068e
safda <safda@LAPTOP-97BJNMRO>
2024-09-28 00:20:52 +0530

    Project Created and committed

---
+++ SpringBootPRJT/src/main/java/com/learn/SpringBootPrjtApplication.java
@@ -0,0 +1,22 @@
+package com.learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.learn.entity.Student;
+
+import repo.StudentRepo;
+
+@SpringBootApplication
+public class SpringBootPrjtApplication {
+	@Autowired
+	StudentRepo stRep;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrjtApplication.class, args);
+		System.out.println("Application Started...");
+		
+	}
+
+}
